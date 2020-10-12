b5d7f980025af88186e1929540919f2fdbfee57e
Timothée Nicole <timothee.nicole@gmail.com>
2020-10-12 15:27:39 +0200

    another way to diplay the card, less beautiful still

--- client/src/components/Mapbox.jsx
+++ client/src/components/Mapbox.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
- 
-class Map = ReactMapboxGl({
-  accessToken:
-    process.env.REACT_APP_MAPBOX_TOKEN
+import React  from 'react';
+import ReactMapGL from 'react-map-gl';
 
-  
-});
+class Map extends React.Component {
 
-export default Map;
+  state = {
+    viewport: {
+      width: 100,
+      height: 100,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8
+    }
+  };
+
+  render() {
+    return (
+    //   <ReactMapGL
+    //     {...this.state.viewport}
+    //     onViewportChange={(viewport) => this.setState({viewport})}
+    //    mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}/>
+    // );
+  }
+}
+
+export default Map
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,31 +1,13 @@
 import React from "react";
 import Map from "../components/Mapbox.jsx";
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+// import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 
 const Home = (props) => {
   // Implement react map box here.
   return (
     <div>
       <h1>MAPBOX MAP HERE </h1>
-      <Map
-  style="mapbox://styles/mapbox/streets-v9"
-  containerStyle={{
-    height: '100vh',
-    width: '100vw'
-  }}
->
- <Map
-  style="mapbox://styles/mapbox/streets-v9"
-  containerStyle={{
-    height: '100vh',
-    width: '100vw'
-  }}
->
-  <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-    <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-  </Layer>
-</Map>;
-</Map>;
+      <Map mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN} mapboxApiUrl={`?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
     </div>
   );
 };
