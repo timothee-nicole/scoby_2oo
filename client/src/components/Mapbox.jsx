43b5617bcd27a30e0706f3d7170aed21ad1f3aa3
sechs <philippe.buteau@skema.edu>
2020-10-12 20:14:56 +0200

    last push end of day

--- client/src/components/Mapbox.jsx
+++ client/src/components/Mapbox.jsx
@@ -1,26 +1,26 @@
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
-import React, { Component } from 'react'
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import React, { Component } from "react";
+import apiHandler from "../api/apiHandler.js";
 
 const Map = ReactMapboxGl({
-    accessToken:
-      'pk.eyJ1IjoidGltb3RoZWVuaWNvbGUiLCJhIjoiY2tnNmVpcWQ3MDFiczMybW5hYm5odXZpbyJ9.Odhtcbzob0s3yNX5jMEKBw'
-  });
+  accessToken:
+    "pk.eyJ1IjoidGltb3RoZWVuaWNvbGUiLCJhIjoiY2tnNmVpcWQ3MDFiczMybW5hYm5odXZpbyJ9.Odhtcbzob0s3yNX5jMEKBw",
+});
 
 export default class Mapbox extends Component {
-
-    render() {
-        return (
-            <Map
-  style="mapbox://styles/mapbox/streets-v9"
-  containerStyle={{
-    height: '100vh',
-    width: '100vw'
-  }}
->
-  <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-    <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-  </Layer>
-</Map>
-        )
-    }
+  render() {
+    return (
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
+          <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+        </Layer>
+      </Map>
+    );
+  }
 }
